79f9ffade53df2d6304ac636b356ea58aa4a0362
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-14 09:02:44 +0200

    create  model migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        CANDIDATE = "CANDIDATE", "Candidate"
+        COMPANY = "COMPANY", "Company"
+        ADMIN = "ADMIN", "Admin"
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CANDIDATE,
+    )
+
+    def __str__(self):
+        return self.username
