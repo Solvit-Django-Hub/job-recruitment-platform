56d96cd5b2ea602de02399b02e2738f978161cfe
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-14 15:25:46 +0200

    feat: add login serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 from .models import User
@@ -29,3 +30,31 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get("username")
+        password = attrs.get("password")
+
+        user = authenticate(
+            username=username,
+            password=password,
+        )
+
+        if user is None:
+            raise serializers.ValidationError(
+                "Invalid username or password."
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                "This account is inactive."
+            )
+
+        attrs["user"] = user
+
+        return attrs
