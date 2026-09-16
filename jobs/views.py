52cad6fc86b6eee74687886228a6a1517adbda1b
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-16 14:57:51 +0200

    add job search and pagination

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import filters, generics
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Job
@@ -6,9 +6,33 @@ from .serializers import JobSerializer
 
 
 class JobListCreateView(generics.ListCreateAPIView):
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
     permission_classes = [IsAuthenticated]
+
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    search_fields = [
+        "title",
+        "description",
+        "requirements",
+        "location",
+        "employment_type",
+        "company__name",
+    ]
+
+    ordering_fields = [
+        "created_at",
+        "salary",
+        "application_deadline",
+    ]
+
+    ordering = ["-created_at"]
+
+    def get_queryset(self):
+        return Job.objects.filter(is_active=True)
 
     def perform_create(self, serializer):
         company = self.request.user.company
